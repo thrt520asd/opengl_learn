5311225eab6e73df67fc7219b79474d0778c7bc2
thrt <610162816@qq.com>
2021-04-20 13:56:16 +0800

    shader param in out uniform

--- main.cpp
+++ main.cpp
@@ -10,20 +10,25 @@ void framebuffer_size_callback(GLFWwindow* window,int width,int height){
 float vertices[] = {
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+     0.0f,  0.5f, 0.0f,
+     
 };
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   vertexColor = vec4(0.5f,0.0,0.0,1.0);"
     "}\0";
 const char *fragShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
+"uniform vec4 color;\n"
 "void main()\n"
 "{\n"
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
-"}\n";
+    "FragColor = color;\n"
+"}\0";
 void PreDraw(){
     
 }
@@ -74,9 +79,23 @@ int main(void)
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
     glCompileShader(vertexShader);
+    int v_success;
+    char v_infoLog[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&v_success);
+    if(!v_success){
+         glGetShaderInfoLog(vertexShader, 512, NULL, v_infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << v_infoLog << std::endl;
+    }
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader,1,&fragShaderSource,NULL);
     glCompileShader(fragmentShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS,&success);
+    if(!success){
+         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram,vertexShader);
     glAttachShader(shaderProgram,fragmentShader);
@@ -94,7 +113,11 @@ int main(void)
         /* Render here */
         glClearColor(0.2f,0.3f,0.3f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        float timeValue = glfwGetTime();
+        float greenValue =(sin(timeValue)/2.0f+0.5f);
+        int vertexColorLocation = glGetUniformLocation(shaderProgram,"color");
         glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation,greenValue,greenValue,greenValue,1.0f);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES,0,3);
         /* Swap front and back buffers */
